c4807864c6370e874caf5519b0ba056ca9bd5509
iimadeddine <imadeddinemarkabi@gmail.com>
2019-06-06 17:43:47 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {LocationStrategy, CommonModule, HashLocationStrategy} from '@angular/common';
+
+import { AppComponent } from './app.component';
+
+// Routing Module
+import { AppRoutingModule } from './app.routing';
+import { FullLayoutComponent} from './layout/full-layout.component';
+import {Ng2AutoBreadCrumb} from "ng2-auto-breadcrumb";
+import {Ng2NewsListComponent} from "./components/ng2-newslist/ng2newslist.component";
+import { UserComponent } from './user/user.component';
+
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CommonModule,
+    Ng2AutoBreadCrumb
+  ],
+  declarations: [
+    FullLayoutComponent,
+    AppComponent,
+    UserComponent
+  ],
+  providers: [{
+    provide: LocationStrategy,
+    useClass: HashLocationStrategy
+  }],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
